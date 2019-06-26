f4e2ca0c9011e01a0972223bd70fce5f206f96b4
Micael Verissimo <micaelvero@hotmail.com>
2019-06-26 16:25:19 -0300

    fix the setup.sh; models.py using tensorflow.

--- python/models.py
+++ python/models.py
@@ -1,25 +1,23 @@
 __all__ = ['create_mlp_model']
 
 import numpy as np
-
-from keras.models import Sequential
-from keras.layers import Dense
+import tensorflow as tf
 
 def create_mlp_model(input_dim, output_dim,
                         n_neurons,
-                        hl_act_func = 'relu',
-                        ol_act_func = 'softmax',
+                        hl_act_func = tf.nn.relu,
+                        ol_act_func = tf.nn.softmax,
                         n_layers=1,
                         loss_func = 'mean_squared_error',
                         optimizer = 'adam',
                         metrics = None,
                         name='model'):
     # Create model
-    model = Sequential(name=name)
+    model = tf.keras.models.Sequential(name=name)
     for ilayer in range(n_layers):
-        model.add(Dense(n_neurons if type(n_neurons) != type([]) else n_neurons[ilayer],
-                        input_dim=input_dim, activation=hl_act_func))
-    model.add(Dense(output_dim, activation=ol_act_func))
+        model.add(tf.keras.layers.Dense(n_neurons if type(n_neurons) != type([]) else n_neurons[ilayer],
+                        input_dim=input_dim, activation=hl_act_func, name='layer_%i' %(ilayer+1) ))
+    model.add(tf.keras.layers.Dense(output_dim, activation=ol_act_func, name='output_layer'))
 
     # Compile model
     model.compile(loss=loss_func,
